f08b19105c819472513f24f4852c5bfff79131e2
Anny <anny@annydeair.home>
2023-03-26 17:27:28 +0200

    add the checksum to end the game

--- connect-four/run.py
+++ connect-four/run.py
@@ -2,7 +2,7 @@ import pathlib
 from typing import Dict, List, Any
 
 import numpy as np
-import pandas as pd
+
 
 BOARD_ROW = 6
 BOARD_COL = 7
@@ -28,17 +28,59 @@ def execute(board: List[Any], col: int, row: int, player: str) -> None:
     Return:
         None, update the board with the color
     """
-    board[row][col] = player
 
-    print("check player and board: ", player, board)
+    board[row][col] = int(player[-1])
 
-
-# def parse_example_input()
+    print("check player and board: ", player, "\n", board)
 
 
-def check_winning():
-    # if end game update game_finish to true
-    return False
+def check_winning(board: List[str], player: str) -> bool:
+    # get player number
+    player_num = int(player[-1])
+    # check the vertical is winning or not
+    for c in range(BOARD_COL - 3):
+        for r in range(BOARD_ROW):
+            if sum(board[r][c : c + 3]) == player_num * 4:
+                print(f"PLAYER {player_num} WIN!!!!")
+                return True
+    print("vertical check finish!")
+    # chec the horizontal is winning or not
+    for c in range(BOARD_COL):
+        for r in range(BOARD_ROW - 3):
+            check_sum = (
+                board[r][c] + board[r + 1][c] + board[r + 2][c] + board[r + 3][c]
+            )
+            if check_sum == player_num * 4:
+                print(f"PLAYER {player_num} WIN!!!!")
+                return True
+
+    print("horizontal check finish!")
+
+    # Check positively sloped diaganols
+    for c in range(BOARD_COL - 3):
+        for r in range(BOARD_ROW - 3):
+            check_sum = (
+                board[r][c]
+                + board[r + 1][c + 1]
+                + board[r + 2][c + 2]
+                + board[r + 3][c + 3]
+            )
+            if check_sum == player_num * 4:
+                return True
+    print("positively sloped diaganols check finish!")
+    # Check negatively sloped diaganols
+    for c in range(BOARD_COL - 3):
+        for r in range(3, BOARD_ROW):
+            check_sum = (
+                board[r][c]
+                + board[r - 1][c + 1]
+                + board[r - 2][c + 2]
+                + board[r - 3][c + 3]
+            )
+            if check_sum == player_num * 4:
+                return True
+
+    print("negatively sloped diaganols check finish!")
 
 
 def parse_matchdata(dataset: List[List[str]]) -> Dict[str, List[str]]:
@@ -78,6 +120,22 @@ def parse_matchdata(dataset: List[List[str]]) -> Dict[str, List[str]]:
     return match_dict
 
 
+def get_next_available_row(board: List[Any], col: int) -> int:
+    """Get the next available row of the column
+
+    Args:
+        board: current board status
+        col: col of the board need to get the available row
+
+    Return:
+        available row position
+
+    """
+    for i in range(BOARD_ROW):
+        if board[i][col] == 0:
+            return i
+
+
 def play_match(players: str, movements: List[str]) -> None:
 
     print("check players: ", players)
@@ -89,17 +147,30 @@ def play_match(players: str, movements: List[str]) -> None:
 
     while not game_finish:
         player1, player2 = players.split(" ")
-
+        turn = 0
         for movement in movements:
-            turn = 0
             if turn == 0:
                 # player 1 play
-                execute(board=board, col=int(movement[-1]) - 1, row=1, player=player1)
-                check_winning()
+                # get the column of the move
+                execute_column = int(movement[-1]) - 1
+                execute(
+                    board=board,
+                    col=execute_column,
+                    row=get_next_available_row(board=board, col=execute_column),
+                    player=player1,
+                )
+                game_finish = check_winning(board=board, player=player1)
             else:
                 # player 2 play
-                execute(board=board, col=int(movement[-1]) - 1, row=1, player=player2)
-                check_winning()
+                # get the column of the move
+                execute_column = int(movement[-1]) - 1
+                execute(
+                    board=board,
+                    col=execute_column,
+                    row=get_next_available_row(board=board, col=execute_column),
+                    player=player2,
+                )
+                game_finish = check_winning(board=board, player=player2)
             turn += 1
             turn = turn % 2
 
@@ -117,7 +188,7 @@ def run():
     game_list = match_dict.keys()
 
     for game in game_list:
-        play_match(players=game, movement=match_dict[game])
+        play_match(players=game, movements=match_dict[game])
 
     return []
 
