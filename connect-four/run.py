c931d811ed8cde68e8c3801212f8baff4c72a390
Anny <anny@annydeair.home>
2023-03-26 21:23:52 +0200

    change the parser and some fixes to run all the matches

--- connect-four/run.py
+++ connect-four/run.py
@@ -1,185 +1,176 @@
 import pathlib
-from typing import Dict, List, Any
-
+from typing import List, Any
 import numpy as np
 
 
-ROW_NUM = 6
-BOARD_ROW = [-1 for _ in range(ROW_NUM)]
-COL_NUM = 7
+BOARD_ROW = 6
+BOARD_COL = 7
 CURRENT_FOLDER = pathlib.Path(__file__).parent
 MATCH_DATA_PATH = CURRENT_FOLDER / "matchdata.txt"
 
 
 def creat_board():
     """create the empty board"""
-    board = [BOARD_ROW for _ in range(COL_NUM)]
+    board = np.zeros((BOARD_ROW, BOARD_COL))
     return board
 
 
 def execute(board: List[Any], col: int, row: int, player: str) -> None:
     """Execute the movement
-
     Args:
         board: the current board
         col: colum of movement
         row: row of movement
         player: color represents the player
-
     Return:
         None, update the board with the color
     """
 
-    board[row][col] = player
+    board[row][col] = int(player[-1])
 
 
-def check_winning(board: List[str], player: str) -> int:
-
+def check_winning(board: List[str], player: str) -> bool:
+    # get player number
+    player_num = int(player)
     # check the vertical is winning or not
-    for c in range(COL_NUM - 3):
-        for r in range(ROW_NUM):
+    for c in range(BOARD_COL - 3):
+        for r in range(BOARD_ROW):
             mask = (
-                board[r][c] == player
-                and board[r][c + 1] == player
-                and board[r][c + 2] == player
-                and board[r][c + 3] == player
+                board[r][c] == player_num
+                and board[r][c + 1] == player_num
+                and board[r][c + 2] == player_num
+                and board[r][c + 3] == player_num
             )
             if mask:
-                print(f"{player} WIN!!!!")
-                print(board)
-                return player
+                print(f"PLAYER {player_num} WIN!!!!")
+                return True
     # chec the horizontal is winning or not
-    for c in range(COL_NUM):
-        for r in range(ROW_NUM - 3):
+    for c in range(BOARD_COL):
+        for r in range(BOARD_ROW - 3):
             mask = (
-                board[r][c] == player
-                and board[r + 1][c] == player
-                and board[r + 2][c] == player
-                and board[r + 3][c] == player
+                board[r][c] == player_num
+                and board[r + 1][c] == player_num
+                and board[r + 2][c] == player_num
+                and board[r + 3][c] == player_num
             )
             if mask:
-                print(f"{player} WIN!!!!")
-                print(board)
-                return player
+                print(f"PLAYER {player_num} WIN!!!!")
+                return True
 
     # Check positively sloped diaganols
-    for c in range(COL_NUM - 3):
-        for r in range(ROW_NUM - 3):
+    for c in range(BOARD_COL - 3):
+        for r in range(BOARD_ROW - 3):
             mask = (
-                board[r][c] == player
-                and board[r + 1][c + 1] == player
-                and board[r + 2][c + 2] == player
-                and board[r + 3][c + 3] == player
+                board[r][c] == player_num
+                and board[r + 1][c + 1] == player_num
+                and board[r + 2][c + 2] == player_num
+                and board[r + 3][c + 3] == player_num
             )
             if mask:
-                print(f"{player} WIN!!!!")
-                print(board)
-                return player
+                print(f"PLAYER {player_num} WIN!!!!")
+                return True
 
     # Check negatively sloped diaganols
-    for c in range(COL_NUM - 3):
-        for r in range(3, ROW_NUM):
+    for c in range(BOARD_COL - 3):
+        for r in range(3, BOARD_ROW):
             mask = (
-                board[r][c] == player
-                and board[r - 1][c + 1] == player
-                and board[r - 2][c + 2] == player
-                and board[r - 3][c + 3] == player
+                board[r][c] == player_num
+                and board[r - 1][c + 1] == player_num
+                and board[r - 2][c + 2] == player_num
+                and board[r - 3][c + 3] == player_num
             )
             if mask:
-                print(f"{player} WIN!!!!")
-                print(board)
-                return player
+                print(f"PLAYER {player_num} WIN!!!!")
+                return True
 
 
-def parse_matchdata(dataset: List[List[str]]) -> Dict[str, List[str]]:
+def parse_matchdata(dataset: List[List[str]]) -> List:
     """Parse the matchdata to dictionary
-
-    This function helps to parse the matchdata into dictionary format.
-    It returns the dictionary with players as key and match movement as values.
-    e.g.
-        match_dict = {
-            "player_8 player_4":
-                ['R3', 'B6', 'R2', 'B3', 'R3', 'B7', 'R5'...]
-        }
+    This function helps to parse the matchdata into two lists.
+    One is for the players and another one is for the movement, given
+    the order won't be altered, simply return two lists.
 
     Args:
         dataset: matchdata.txt dataset in list format
-
     Return:
-        dictionary of matchdata
+        players list and movement list
     """
     players = []
     movement = []
-    match_dict = {}
 
     for data in dataset:
         if len(data) == 2:
             # make the player A and B as key for the match dictionary
-            players.append(f"{data[0]} {data[1]}")
+            player1 = data[0].split("_")[-1]
+            player2 = data[1].split("_")[-1]
+            players.append(f"{player1} {player2}")
         else:
             # simply append the movement
             movement.append(data)
 
-    # the dataset is ordered by the players with their movement
-    # loop over two list to create a dictionary
-    for i in range(len(players)):
-        match_dict[players[i]] = movement[i]
-
-    return match_dict
+    return players, movement
 
 
 def get_next_available_row(board: List[Any], col: int) -> int:
     """Get the next available row of the column
-
     Args:
         board: current board status
         col: col of the board need to get the available row
-
     Return:
         available row position
-
     """
-
-    for i in range(ROW_NUM):
-        if board[i][col] == -1:
+    for i in range(BOARD_ROW):
+        if board[i][col] == 0:
             return i
 
 
-def play_match(players: str, movements: List[str]) -> str:
-
-    # create a new board
+def play_match(players: str, movements: List[str]) -> int:
 
     board = creat_board()
+    game_finish = False
 
-    player1, player2 = players.split(" ")
-    turn = 0
-    print(player1, player2)
-    for movement in movements:
-        execute_column = int(movement[-1]) - 1
-        if turn == 0:
-            # player 1 play
-            # get the column of the move
-            execute(
-                board=board,
-                col=execute_column,
-                row=get_next_available_row(board=board, col=execute_column),
-                player=player1,
-            )
-            game_winner = check_winning(board=board, player=player1)
-        else:
-            # player 2 play
-            # get the column of the move
-            execute(
-                board=board,
-                col=execute_column,
-                row=get_next_available_row(board=board, col=execute_column),
-                player=player2,
-            )
-            game_winner = check_winning(board=board, player=player2)
-        turn += 1
-        turn = turn % 2
-        if game_winner:
-            return f"Player {game_winner} win"
+    while not game_finish:
+        player1, player2 = players.split(" ")
+        print(player1, player2)
+        if player1 == 0:
+            print(f"{player1} need to be replace")
+            # replace the player 0 with -1
+            player1 = -1
+        elif player2 == 0:
+            print(f"{player2} need to be replace")
+            player2 = -1
+        turn = 0
+        for number, movement in enumerate(movements):
+            if turn == 0:
+                # player 1 play
+                # get the column of the move
+                execute_column = int(movement[-1]) - 1
+                execute(
+                    board=board,
+                    col=execute_column,
+                    row=get_next_available_row(board, execute_column),
+                    player=player1,
+                )
+                game_finish = check_winning(board, player1)
+                if game_finish is True:
+                    return "someonewin"
+            else:
+                # player 2 play
+                # get the column of the move
+                execute_column = int(movement[-1]) - 1
+                execute(
+                    board=board,
+                    col=execute_column,
+                    row=get_next_available_row(board, execute_column),
+                    player=player2,
+                )
+                game_finish = check_winning(board, player2)
+                if game_finish is True:
+                    return "someonewin"
+            turn += 1
+            turn = turn % 2
+            if number == 41:
+                return "no one win"
 
 
 def run():
@@ -190,13 +181,14 @@ def run():
     data_list = [item.split(",") for item in f.read().splitlines() if item != ""]
 
     # parse the matchdata to dictionary
-    match_dict = parse_matchdata(data_list)
+    player_list, movement_list = parse_matchdata(data_list)
 
-    game_list = match_dict.keys()
+    winner_list = []
+    for num in range(len(player_list)):
+        winner = play_match(players=player_list[num], movements=movement_list[num])
+        winner_list.append(winner)
 
-    for game in game_list:
-        winner = play_match(players=game, movements=match_dict[game])
-        print(winner)
+    print("final winner list: ", len(winner_list))
 
 
 if __name__ == "__main__":
